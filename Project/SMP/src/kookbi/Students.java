076ebcea60bb4ded60f9deeb19ea3b2c59c02af4
mingJ7235 <joshuara7235@gmail.com>
2020-11-24 15:52:58 +0900

    project commit

---
+++ Project/SMP/src/kookbi/Students.java
@@ -0,0 +1,39 @@
+package kookbi;
+
+public class Students {
+	
+	private String name;
+	private int age;
+	private int number;
+	private static int Fnum = 20201000;
+
+	public Students() {
+	}
+	public Students(String name, int age) {
+		this.name = name;
+		this.age = age;
+		this.number = ++Fnum;
+	}
+		public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	String show () {
+		return "학번 : "+ this.number + " 이름 : " + this.name + " 나이 : " + age + "살";
+	}	
+}
