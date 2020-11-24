d102616d3f36b35645f55afba6254b5fd94f2d75
mingJ7235 <joshuara7235@gmail.com>
2020-11-24 23:12:12 +0900

    Changed!

--- Project/SMP2/src/kookbi/Library.java
+++ Project/SMP2/src/kookbi/Library.java
@@ -2,7 +2,22 @@ package kookbi;
 
 public class Library {
 	//도서관
-	public void read () {
+	//책을 리스트업하고,
+	//빌려간 학생 목록을 검색할 수 있음
+	
+	
+	public void listup () {
+		
+	}
+	
+	//책 대여
+	public void rent () {
+		
+		
+	}
+	
+	//책 대여 학생 리스트
+	public void rentList () {
 		
 	}
 }
---
+++ Project/SMP2/src/kookbi/School.java
@@ -0,0 +1,15 @@
+package kookbi;
+
+import javax.swing.JOptionPane;
+
+public class School {
+	String [] arMode = {"도서관 관리 모드", "학생 관리 모드", "교사 관리 모드"};
+	int choice = 0;
+	public void MainView () {
+		JOptionPane.showOptionDialog(null, "[코리아 IT 아카데미에 오신것을 환영합니다]\n"
+				+ "아래의 메뉴를 선택하여 접근할 모드를 선택해주세요", "학생부 관리 Menu", JOptionPane.DEFAULT_OPTION, 
+				JOptionPane.PLAIN_MESSAGE, null/*이미지*/, arMode, null);		
+		
+		
+	}
+}
