b022a8220eae8088b7a12011df9de77bba502c59
Johsua_Kim <joshuara7235@gmail.com>
2020-12-03 00:31:22 +0900

    java.time package commit

--- This_Is_JAVA/11-16_java_time_package/src/p1/TimePackageEx.java
+++ This_Is_JAVA/11-16_java_time_package/src/p1/TimePackageEx.java
@@ -1,7 +1,28 @@
 package p1;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class TimePackageEx {
 	public static void main(String[] args) {
 		//Time Package는 자바 1.8버전 이후부터 API에서 지원해줌 
+		
+		//날짜 얻기
+		
+		LocalDate currDate = LocalDate.now(); 
+		System.out.println("Local date : " + currDate);
+		
+		LocalDate targetDate = LocalDate.of(2024, 5, 12);
+		System.out.println("2024년 5월 12일 : "+targetDate);
+		
+		//시간 얻기
+		LocalTime currTime = LocalTime.now();
+		System.err.println("현재시간 : " + currTime);
+		
+		LocalTime targetTime = LocalTime.of(6, 30);
+		System.out.println(targetTime);
+		
+		//날짜와 시간 얻기
+		
 	}
 }
