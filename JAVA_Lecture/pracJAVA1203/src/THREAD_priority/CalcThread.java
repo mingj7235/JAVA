96e12ae96e23896808a59b6bbfadf072bb8283ec
Johsua_Kim <joshuara7235@gmail.com>
2020-12-03 18:50:58 +0900

    some modify

--- JAVA_Lecture/pracJAVA1203/src/THREAD_priority/CalcThread.java
+++ JAVA_Lecture/pracJAVA1203/src/THREAD_priority/CalcThread.java
@@ -1,5 +1,18 @@
 package THREAD_priority;
 
-public class CalcThread {
+public class CalcThread extends Thread{
+	
+	private String name;
+	
+	public CalcThread(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
 
 }
