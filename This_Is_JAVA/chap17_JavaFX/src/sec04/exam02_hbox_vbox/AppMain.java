45a1c92caa1f582a4527dc04068147487b99b6d7
Johsua_Kim <joshuara7235@gmail.com>
2020-12-12 23:39:08 +0900

    FXML making

---
+++ This_Is_JAVA/chap17_JavaFX/src/sec04/exam02_hbox_vbox/AppMain.java
@@ -0,0 +1,31 @@
+package sec04.exam02_hbox_vbox;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application{
+	//자바코드로 모두 만들어내는것 -> 프로그램적 레이아웃
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
+		//이 한줄로 레이아웃이 생성이됨. 자바코드로 작성하는것이 다  빠져버림. 
+		//xml 파일 안에있는 내용을 바탕으로 parent라는 루트 컨테이너를 만든다. 라는 의미임. 
+		
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("AppMain");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+				
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	
+}
