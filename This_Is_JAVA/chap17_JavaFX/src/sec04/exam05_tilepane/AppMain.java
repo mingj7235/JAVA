5f5f1648af5d3811e2fc0ea6291ebede33a54788
Johsua_Kim <joshuara7235@gmail.com>
2020-12-13 21:29:59 +0900

    flowpane, tilepane

--- This_Is_JAVA/chap17_JavaFX/src/sec04/exam05_tilepane/AppMain.java
+++ This_Is_JAVA/chap17_JavaFX/src/sec04/exam05_tilepane/AppMain.java
@@ -1,4 +1,4 @@
-package sec04.exam03_borderpane;
+package sec04.exam05_tilepane;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,17 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class AppMain extends Application {
-
+public class AppMain extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root =FXMLLoader.load(getClass().getResource("root.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
 		Scene scene = new Scene(root);
 		
 		primaryStage.setTitle("AppMain");
 		primaryStage.setScene(scene);
-		primaryStage.setWidth(300);
-		primaryStage.setHeight(200);
 		primaryStage.show();
 	}
 	public static void main(String[] args) {
