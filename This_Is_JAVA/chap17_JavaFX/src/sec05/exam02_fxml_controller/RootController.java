755586fba5e824a34a94fe9a6b8019f04b38302d
Johsua_Kim <joshuara7235@gmail.com>
2020-12-14 00:16:28 +0900

    modify

--- This_Is_JAVA/chap17_JavaFX/src/sec05/exam02_fxml_controller/RootController.java
+++ This_Is_JAVA/chap17_JavaFX/src/sec05/exam02_fxml_controller/RootController.java
@@ -12,7 +12,7 @@ import javafx.scene.control.Button;
 public class RootController implements Initializable{
 	
 	@FXML private Button btn1;
-	@FXML private Button btn2;
+	//btn2는 FXML에서 직접 컨트롤러를 설정해줌
 	@FXML private Button btn3;
 	
 	
@@ -20,15 +20,12 @@ public class RootController implements Initializable{
 	public void initialize(URL location, ResourceBundle resources) {
 		//컨트롤의 이벤트 처리 코드
 		btn1.setOnAction(event -> System.out.println("버튼1 클릭"));
-		btn2.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("버튼2 클릭");
-			}
-		});
 		btn3.setOnAction(event -> handleBtn3Action(event));
 	}
 	public void handleBtn3Action (ActionEvent event) {
 		System.out.println("버튼3 클릭");
+	}
+	public void handleBtn2Action (ActionEvent event) {
+		System.out.println("버튼2 클릭");
 	}
 }
