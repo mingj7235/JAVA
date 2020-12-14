9e5152fd55ece8187fa675b02f9426c25d15817a
Johsua_Kim <joshuara7235@gmail.com>
2020-12-14 10:57:05 +0900

    property study

---
+++ This_Is_JAVA/chap17_JavaFX/src/sec06/exam02_property_bind/RootController.java
@@ -0,0 +1,20 @@
+package sec06.exam02_property_bind;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+public class RootController implements Initializable{
+	@FXML private TextArea textArea1;
+	@FXML private TextArea textArea2;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Bindings.bindBidirectional(textArea1.textProperty(), textArea2.textProperty());
+		//두 컨트롤의 텍스트 속성이 양방향으로 바인딩이 된것임
+	}
+}
