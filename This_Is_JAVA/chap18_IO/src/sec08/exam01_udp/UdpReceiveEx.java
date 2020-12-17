b96c5fb38fd9cfe38921449192fece3992bc0045
Johsua_Kim <joshuara7235@gmail.com>
2020-12-17 12:08:22 +0900

    server study

--- This_Is_JAVA/chap18_IO/src/sec08/exam01_udp/UdpReceiveEx.java
+++ This_Is_JAVA/chap18_IO/src/sec08/exam01_udp/UdpReceiveEx.java
@@ -1,5 +1,33 @@
 package sec08.exam01_udp;
 
-public class UdpReceiveEx {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
+public class UdpReceiveEx {
+	public static void main(String[] args) throws Exception{
+		DatagramSocket ds = new DatagramSocket (5001); //리시버는 포트번호를 줘서 바인딩 시켜줘야한다.
+		
+		Thread thread = new Thread () {
+			public void run() {
+				System.out.println("[수신 시작]");
+				try {
+					while (true) {
+						DatagramPacket packet = new DatagramPacket(
+								new byte [100], 100);
+						ds.receive(packet);
+						
+						String data = new String (packet.getData(), 0, packet.getLength(), "UTF-8");
+						System.out.println("[받은 내용 : " + packet.getSocketAddress() + "]" + data);
+					}
+				}catch (Exception e) {
+					System.out.println("[수신 종료]");
+				}
+			}
+		};
+		
+		thread.start();
+		
+		Thread.sleep(10000); //10초 이후에 클로즈시키는 것임
+		ds.close();
+	}
 }
--- This_Is_JAVA/chap18_IO/src/sec08/exam01_udp/UdpSendEx.java
+++ This_Is_JAVA/chap18_IO/src/sec08/exam01_udp/UdpSendEx.java
@@ -1,5 +1,27 @@
 package sec08.exam01_udp;
 
-public class UdpSendEx {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
 
+public class UdpSendEx {
+	public static void main(String[] args) throws Exception{
+		DatagramSocket ds = new DatagramSocket();
+		
+		System.out.println("[발신시작]");
+		
+		for (int i = 0; i < 3; i++) {
+			String data = "메세지" + (i+1);
+			byte [] byteArr = data.getBytes("UTF-8");
+			DatagramPacket packet = new DatagramPacket(
+					byteArr, byteArr.length, new InetSocketAddress("localhost", 5001)
+					);
+			
+			ds.send(packet);
+			System.out.println("보낸 바이트 수:" +byteArr.length + " bytes");
+		}
+		System.out.println("[발신 종료]");
+		ds.close();
+		
+	}
 }
