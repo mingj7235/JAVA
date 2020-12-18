6fdbd62206da31e653dd36ac7f1f128979244dd2
Johsua_Kim <joshuara7235@gmail.com>
2020-12-18 12:59:21 +0900

    Stream study

--- JAVA_Lecture/pracJAVA1218/src/p03/StreamPipeLineEx.java
+++ JAVA_Lecture/pracJAVA1218/src/p03/StreamPipeLineEx.java
@@ -1,5 +1,63 @@
 package p03;
 
-public class StreamPipeLineEx {
+import java.util.Arrays;
+import java.util.List;
 
+public class StreamPipeLineEx {
+	public static void main(String[] args) {
+		List <Member> list = Arrays.asList(
+					new Member ("홍길동", Member.Male, 30),
+					new Member ("김나리", Member.Female, 20),
+					new Member ("신용권", Member.Male, 45),
+					new Member ("박수미", Member.Female, 27)
+				);
+		
+		long maleCount = list.stream().filter(m -> m.getSex() == Member.Male).mapToInt(Member::getAge).count();
+		System.out.println("남자 수 : " + maleCount);
+		long femaleCount = list.stream().filter(m -> m.getSex() == Member.Male).mapToInt(Member::getAge).count();
+		System.out.println("여자 수 : " + femaleCount);
+		double femaleAgeAvg = list.stream().filter(m -> m.getSex() == Member.Male).mapToInt(Member::getAge).average().getAsDouble();
+		System.out.println(femaleAgeAvg);
+		
+	}
+}
+class Member{
+	public static int Male = 0;
+	public static int Female = 1;
+	private String name;
+	private int sex;
+	private int age;
+
+	public Member(String name, int sex, int age) {
+		this.name = name;
+		this.sex = sex;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
 }
