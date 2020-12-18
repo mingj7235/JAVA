72c3897c10c22495d64b3646918f27dfcb90cc8d
Johsua_Kim <joshuara7235@gmail.com>
2020-12-18 14:53:02 +0900

    box study

--- JAVA_Lecture/pracJAVA1218/src/p05/MapEx.java
+++ JAVA_Lecture/pracJAVA1218/src/p05/MapEx.java
@@ -2,9 +2,20 @@ package p05;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class MapEx {
 	public static void main(String[] args) {
+		int [] intArray = {1,2,3,4,5};
+		IntStream intStream = Arrays.stream(intArray);
+		
+		intStream.asDoubleStream().forEach(n -> System.out.println(n));
+		
+		intStream = Arrays.stream(intArray);
+		intStream.boxed().forEach(n -> System.out.println(n.intValue()));
+		
+		
+		
 		List <Student> studentList = Arrays.asList( 
 				new Student("홍길동", 10),
 				new Student("신용권", 20),
