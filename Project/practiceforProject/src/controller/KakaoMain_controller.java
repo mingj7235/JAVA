0e8aba58f531bc8808f3c930307e179dfb91d9b2
mingJ7235 <joshuara7235@gmail.com>
2021-01-02 02:04:28 +0900

    Button Connection for ActionEvent :herb:

--- Project/practiceforProject/src/controller/KakaoMain_controller.java
+++ Project/practiceforProject/src/controller/KakaoMain_controller.java
@@ -4,20 +4,22 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.*;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.*;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.*;
+import javafx.stage.Stage;
 
 public class KakaoMain_controller implements Initializable{
 	@FXML private Button kakaoMain_login_btn;
-	@FXML private Button kakaoMain_signin_btn;
 	@FXML private TextField KakaoMain_login_email;
-	@FXML private TextField KakaoMain_login_password;
+	@FXML private PasswordField KakaoMain_login_password;
+
+	@FXML private Button kakaoMain_signin_btn;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -26,14 +28,22 @@ public class KakaoMain_controller implements Initializable{
 			
 	}
 	public void handleBtnLogin (ActionEvent event) {
-		try {
-			Parent login = FXMLLoader.load(getClass().getClassLoader().getResource("view/Friends.fxml"));
-			Scene scene = new Scene(login);
-			Stage primaryStage = (Stage) kakaoMain_login_btn.getScene().getWindow();
-			primaryStage.setScene(scene);
-		} catch (Exception e) {
-			e.printStackTrace();
+		String id = KakaoMain_login_email.getText();
+		String pw = KakaoMain_login_password.getText();
+		
+		if (id.equals("01064707235") && pw.equals("7235")) {
+			try {
+				Parent login = FXMLLoader.load(getClass().getClassLoader().getResource("view/Friends.fxml"));
+				Scene scene = new Scene(login);
+				Stage primaryStage = (Stage) kakaoMain_login_btn.getScene().getWindow();
+				primaryStage.setScene(scene);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("로그인실패");
 		}
+		
 		
 	}
 	
