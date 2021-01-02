e68feaf0c0d865f56b6b046812b67b1e4a1a9317
mingJ7235 <joshuara7235@gmail.com>
2021-01-02 12:17:12 +0900

    Friends screen update for navigation bar :smiley:

---
+++ Project/practiceforProject/src/controller/Signin_controller.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Signin_controller implements Initializable{
+	@FXML private TextField signin_siginin_phonenum;
+	@FXML private PasswordField signin_signin_password;
+	@FXML private PasswordField signin_lsignin_passwordcheck;
+
+	@FXML private Button signin_signin_btn;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		signin_signin_btn.setOnAction(e->handleBtnSignin(e));
+			
+	}
+	public void handleBtnSignin (ActionEvent event) {
+		//db에 저장해야함
+		String id = signin_siginin_phonenum.getText();
+		String pw = signin_signin_password.getText();
+		String pwcheck = signin_lsignin_passwordcheck.getText();
+		
+		if (!(id.isEmpty()) && pw.equals(pwcheck)) {
+			try {
+				Parent login = FXMLLoader.load(getClass().getClassLoader().getResource("view/Login.fxml"));
+				Scene scene = new Scene(login);
+				Stage primaryStage = (Stage) signin_signin_btn.getScene().getWindow();
+				primaryStage.setScene(scene);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("로그인실패");
+		}
+		
+		
+	}
+	
+	
+}
