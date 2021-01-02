ac24da29bf8fe0f1b95b3e7254c618d7c9242905
mingJ7235 <joshuara7235@gmail.com>
2021-01-02 21:22:53 +0900

    References for chat program

--- Project/practiceforProject/src/view/DB_default.java
+++ Project/practiceforProject/src/view/DB_default.java
@@ -6,32 +6,19 @@ import java.sql.SQLException;
 //연결 성공 완료 확인
 public class DB_default {
 	
-	public static void main(String[] args) {
+	public DB_default() {
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String userid="system";
 		String passwd ="1234";
 		
 		try {
-
-		     Class.forName(driver);
-		     System.out.println("드라이브 연결 성공함");//2단계
-		     DriverManager.getConnection(url, userid, passwd);
-		     System.out.println("오라클 연결 성공함");//3단계
-
-		    } catch (ClassNotFoundException e) {
-
-		     System.out.println("드라이브 연결 실패함");
-		     e.printStackTrace();
-		    } catch (SQLException e) {
-
-		     System.out.println("오라클 연결 실패함");
-
-		     e.printStackTrace();
-
-		    }//3단계
-
-		   }
-	
-	
+			Class.forName(driver);
+			DriverManager.getConnection(url, userid, passwd);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
